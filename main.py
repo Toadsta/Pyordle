38a097a9cd5b35a5dbc6c0e8fd95d97e4a0a8afe
Toby <t.f.taylor1@ncl.ac.uk>
2022-02-17 20:33:20 +0000

    update - updated main.py to separate concerns slightly, ideally I need to separate more, also turned the game into a function

--- main.py
+++ main.py
@@ -2,37 +2,43 @@ import random
 from word_getter import data
 from word_size_getter import list_by_size
 
-size = 5  # input("How long of a word would you like to guess?")
 
-word_list = list_by_size(data, int(size))
+def game(size, data):
+    word_list = list_by_size(data, int(size))
 
-chosen_word = random.choice(word_list)
+    chosen_word = random.choice(word_list)
 
-guesses_remaining = size
+    guesses_remaining = size
 
-split_word = list(chosen_word)
+    split_word = list(chosen_word)
 
-print(chosen_word)
-won = False
-while guesses_remaining > 0 and won == False:
-    guess = input(f"Enter a {size} letter word: ")
-    split_guess = list(guess)
-    pos = 0
-    for letter in split_guess:
-        if split_guess == split_word:
-            won = True
+    print(chosen_word)
+    won = False
+    while guesses_remaining > 0 and won == False:
+        guess = input(f"Enter a {size} letter word: ").lower()
+        split_guess = list(guess)
+        pos = 0
+        for letter in split_guess:
+            if split_guess == split_word:
+                won = True
+            elif split_guess[pos] == split_word[pos]:
+                print(f"The letter {letter} is in the correct place ")
+            elif letter in split_word:
+                print(f"The letter {letter} is in the word but not the correct place")
+            else:
+                print(f"The letter {letter} is not in the word")
+            pos += 1
+
+        if won:
             break
-        elif split_guess[pos] == split_word[pos]:
-            print(f"The letter {letter} is in the correct place ")
-        elif letter in split_word:
-            print(f"The letter {letter} is in the word but not the correct place")
         else:
-            print(f"The letter {letter} is not in the word")
-        pos += 1
-    guesses_remaining -= 1
-    print(guesses_remaining)
+            guesses_remaining -= 1
+            print(f"You have {guesses_remaining} guesses remaining")
 
-if guesses_remaining > 0:
-    print("You Won :)")
-else:
-    print("You Lost :(")
+    if guesses_remaining > 0:
+        print("You Won :)")
+    else:
+        print("You Lost :(")
+
+
+game(6, data)
